99edd09d949c617be246f4eff08e6eddcba5913a
Simon Troyanovskiy <floitet.s@outlook.com>
2020-04-06 20:26:05 +0300

    upd

--- lesson_01/task_02_(exercise_03).py
+++ lesson_01/task_02_(exercise_03).py
@@ -7,8 +7,10 @@ y1 = float(input('Type in first dot\'s axis "y" coordinate'))
 x2 = float(input('Type in second dot\'s axis "x" coordinate'))
 y2 = float(input('Type in second dot\'s axis "y" coordinate'))
 
-k = (y1 - y2) / (x1 - x2)
-b = y2 - k * x2
-
-print(f'The equation of the straight line connecting these two dots is:\n'
-      f'y = {k}x + {b}')
+if x1 == x2:
+    print('Unable to calculate k as it is impossible to divide by zero.')
+else:
+    k = (y1 - y2) / (x1 - x2)
+    b = y2 - k * x2
+    print(f'The equation of the straight line connecting these two dots is:\n'
+          f'y = {k}x + {b}')
